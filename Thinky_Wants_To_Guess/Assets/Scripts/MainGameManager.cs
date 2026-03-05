d9bae8e4775ad93bd32129bc13bf6276f79ce084
yesirsenic <yesirsenic@naver.com>
2026-03-05 18:46:42 +0900

    commit
    ai가 제출 기다릴 때 내용 추가

--- Thinky_Wants_To_Guess/Assets/Scripts/MainGameManager.cs
+++ Thinky_Wants_To_Guess/Assets/Scripts/MainGameManager.cs
@@ -8,8 +8,30 @@ public class MainGameManager : MonoBehaviour
     [SerializeField]
     Text suggestWord;
 
+    [SerializeField]
+    Text ai_Text;
+
     private void Start()
     {
         suggestWord.GetComponent<LocalizedSuggestWord>().SetWord(GameManager.Instance.suggestKey);
+
+        StartCoroutine(WaitingDots(ai_Text));
+
+    }
+
+    IEnumerator WaitingDots(Text text, float interval = 0.25f)
+    {
+        string baseText = text.text;
+        int dotCount = 0;
+
+        while (true)
+        {
+            dotCount++;
+            if (dotCount > 3) dotCount = 0;
+
+            text.text = baseText + new string('.', dotCount);
+
+            yield return new WaitForSeconds(interval);
+        }
     }
 }
